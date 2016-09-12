a7c4c134ca30fa3539bffc001a949cf0bc2c1fd2
Aadarsh <aadarsh.sampath@gmail.com>
2016-09-12 12:03:29 -0700

    updating models with quest and answer fields

--- MyPollApp/pollApp/models.py
+++ MyPollApp/pollApp/models.py
@@ -3,3 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+	q_text = models.CharField(max_length=200)
+	publish_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+	question = models.ForeignKey(Question)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
